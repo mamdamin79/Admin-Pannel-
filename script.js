fe8070e18002a069bfc3c9916369cc79c2b3f36c
mohammadaminsaheb <mohammadaminsaheb@gmail.com>
2022-04-03 07:31:19 -0700

    create main section and refactor some prev css

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ const burgerMenu = document.querySelector(".burgerMenu")
 const navBar = document.querySelector("nav")
 const navList = Array.from(document.querySelectorAll(".navLink-text"))
 const logoText = document.querySelector(".logo-text")
-const header = document.querySelector("header")
 const toggleContainer = document.querySelector(".toggle-container")
 const pointerToggle = document.querySelector(".pointer-toggle")
+const container = document.querySelector(".container")
+let users = document.querySelector(".users")
+console.log(users)
 
 
 
@@ -17,15 +19,14 @@ burgerMenu.addEventListener("click",()=>{
     if(navBar.classList.contains("close")){
         navList.map(item => item.classList.add("hidden"))
         logoText.classList.add("hidden")
-        header.classList.add("open")
         toggleContainer.classList.add("close")
         pointerToggle.classList.add("dark")
+        container.classList.add("close")
     }
     else{
         navList.map(item => item.classList.remove("hidden"))
         logoText.classList.remove("hidden")
         logoText.classList.remove("hidden")
-        header.classList.remove("open")
         toggleContainer.classList.remove("close")
     }
 
@@ -34,4 +35,19 @@ burgerMenu.addEventListener("click",()=>{
 pointerToggle.addEventListener("click", ()=>{
     pointerToggle.classList.toggle("dark")
     document.body.classList.toggle("dark")
+})
+//
+window.addEventListener("DOMContentLoaded", ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(usersArray => usersArray.map(user =>{
+        html = `<div class="user">
+                    <p class="userName">${user.name}</p>
+                    <p class="userEmail">${user.email}</p>
+                    <p class="userCompany">${user.company.name}</p>
+                    <p class="userZipCode">${user.address.zipcode}</p>
+                </div>`
+                users.innerHTML += html
+  }
+    ))
 })
